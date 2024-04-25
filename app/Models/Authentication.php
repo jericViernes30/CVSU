b154749537415aeed95feff160bcd81375f13ebb
Jeric James Viernes <jericviernes06@gmail.com>
2024-04-25 15:02:16 +0800

    model for Login

---
+++ app/Models/Authentication.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Authentication extends Authenticatable
+{
+    use HasFactory;
+    protected $table = 'tbl_cashiers';
+
+    protected $fillable = [
+        'name',
+        'password'
+    ];
+
+    public $timestamps = false;
+
+
+}
