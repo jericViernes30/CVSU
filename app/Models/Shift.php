808c02c4383d005c47e863ccc9b87c36f11c5ddc
Jeric James Viernes <jericviernes06@gmail.com>
2024-04-25 15:03:02 +0800

    model for Shift

---
+++ app/Models/Shift.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Shift extends Model
+{
+    use HasFactory;
+    protected $table = 'tbl_shifts';
+
+    protected $fillable = [
+        'cashier',
+        'POS_number',
+        'starting_cash',
+        'closing_cash'
+    ];
+}
