1fc024ce393607163a27048c80751d3cdad12018
Jeric James Viernes <jericviernes06@gmail.com>
2024-06-19 23:51:05 +0800

    added 2 new columns

--- database/migrations/2024_04_23_040954_create_tbl_cashiers.php
+++ database/migrations/2024_04_23_040954_create_tbl_cashiers.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('password');
+            $table->string('role');
+            $table->timestamps();
         });
     }
 
