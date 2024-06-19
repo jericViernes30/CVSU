e6f35cf179d70a355a50bf36732511c11d57e076
Jeric James Viernes <jericviernes06@gmail.com>
2024-06-19 23:50:14 +0800

    added stock() relation

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -8,12 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Ticket extends Model
 {
     use HasFactory;
-    protected $table = 'tbl_tickets';
+    protected $table = 'orders';
 
     public $fillable = [
         'ticket',
         'food_name'
     ];
 
-    public $timestamps = false;
+    public function stock()
+    {
+        return $this->belongsTo(Stocks::class, 'food_name', 'item');
+    }
 }
