5a904a6402a041ca6bbb0bcd54a1c6a74468d170
Jeric James Viernes <jericviernes06@gmail.com>
2024-06-19 23:50:27 +0800

    added 2 middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Middleware\IsAdminLoggedIn;
+use App\Http\Middleware\isCashierLoggedIn;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\Roles;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +14,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'IsAdminLoggedIn' => IsAdminLoggedIn::class,
+            'IsCashierLoggedIn' => isCashierLoggedIn::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
