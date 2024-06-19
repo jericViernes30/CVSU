930df10f87c54df4ed876d099c05ca5286a75f3e
Jeric James Viernes <jericviernes06@gmail.com>
2024-06-19 23:47:10 +0800

    authentication middleware

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,15 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\Authentication;
+use App\Models\Admin;
 use Illuminate\Auth\Middleware\Authenticate;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use App\Models\Shift;
 
 class AuthController extends Controller
 {
-    public function authLogin(Request $request)
+    public function authLoginCashier(Request $request)
     {
+        // dd($request);
         $pos_number = $request->input('POS');
         $request->validate([
             'POS' => 'required',
@@ -21,24 +25,68 @@ class AuthController extends Controller
 
         $credentials = $request->only('name', 'password');
 
-        if(Auth::attempt($credentials)){
-            $cashier_name = Auth::user()->name; // Assuming the user model has a 'name' attribute
-            session(['cashier_name' => $cashier_name]);
-            session(['pos_number' => $pos_number]);
-            return redirect()->intended(route('dashboard'));
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            if($user->role === 'Cashier'){
+                $cashier_name = Auth::user()->name; // Assuming the user model has a 'name' attribute
+                session(['cashier_name' => $cashier_name]);
+                session(['pos_number' => $pos_number]);
+                return redirect()->intended(route('dashboard'));
+            }
         }
-        return redirect(route('welcome'));
+
+        return redirect()
+                ->route('welcome')
+                ->with('error', 'Authentication failed. Please check your credentials.');
     }
 
-    public function addCashier(){
-        $name = 'Jeric';
-        $password = 'jeric';
+    public function authLoginAdmin(Request $request){
+        // Validation
+        $request->validate([
+            'name' => 'required',
+            'password' => 'required'
+        ]);
+    
+        // Extract credentials
+        $credentials = $request->only('name', 'password');
+    
+        // Authentication Attempt
+        if (Auth::guard('admin')->attempt($credentials)) {
+            // dd(Auth::check());
+    
+            // If authenticated, get the user
+            $user = Auth::guard('admin')->user();
+            // dd($user)
+    
+            if($user->role === 'Admin'){
+                session(['admin_name' => $user->name]); // Use $user directly
+                return redirect()->intended(route('office.dashboard'));
+            }
+        }
+        
+        return redirect()
+               ->back()
+               ->with('error', 'Authentication failed. Please check your credentials.');
+    }
+
+    public function adminLogout(Request $request){
+        Auth::guard('admin')->logout();
+        return redirect()->route('office.login');
+    }
+    
+
+
+    public function addCashier(Request $request){
+        $name = $request->input('cashier_name');
+        $password = $request->input('password');
+        $role = "Cashier";
 
         $hashedPassword = Hash::make($password);
 
         $data = ([
             'name' => $name,
-            'password' => $hashedPassword
+            'password' => $hashedPassword,
+            'role' => $role
         ]);
 
         $create = Authentication::create($data);
@@ -46,9 +94,46 @@ class AuthController extends Controller
         if(!$create){
             return view('welcome');
         }
+        
+        $cashiers = Authentication::all();
+        return view('backoffice/cashiers/cashiers', ['cashiers' => $cashiers]);
     }
 
-    public function logout(){
+    public function addAdmin(){
+        $name = "Admin";
+        $password = "admin1";
+        $role = "Admin";
 
+        $hashedPassword = Hash::make($password);
+
+        $data = ([
+            'name' => $name,
+            'password' => $hashedPassword,
+            'role' => $role
+        ]);
+
+        $create = Admin::create($data);
+
+        if(!$create){
+            return view('welcome');
+        }
+        return redirect()->route('office.login');
+    }
+
+    public function endShift(Request $request) // Add Request type hinting
+    {
+        $cashier_name = session('cashier_name');
+        $shift = Shift::where('cashier', $cashier_name)->delete();
+        // Log out the user
+        Auth::logout();
+
+        // Invalidate the session
+        $request->session()->invalidate();
+
+        // Regenerate the session token to prevent CSRF attacks
+        $request->session()->regenerateToken();
+
+        // Redirect to the login page
+        return redirect()->route('welcome'); // Adjust the redirect path as needed
     }
 }
