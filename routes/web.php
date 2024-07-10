0d7b38940c76eb8f3022e476639b9fec482eaf0f
Jeric James Viernes <jericviernes06@gmail.com>
2024-07-10 09:17:22 +0800

    added few routes

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Middleware\isCashierLoggedIn;
 use PhpOffice\PhpSpreadsheet\Calculation\LookupRef\Offset;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('receipt');
 })->name('welcome');
 Route::post('/login', [AuthController::class, 'authLoginCashier'])->name('auth_login');
 
@@ -26,7 +26,12 @@ Route::middleware([isCashierLoggedIn::class])->group(function(){
     Route::get('/history_ticket/{ticket}', [POSController::class, 'historyTicket'])->name('history.ticket');
     Route::post('/end-shift', [AuthController::class, 'endShift'])->name('end-shift');
     Route::post('/cash-management', [POSController::class, 'cashManagement'])->name('cash_management');
-    Route::get('/livesearch/{key}', [POSController::class, 'livesearch'])->name('livesearch');
+    Route::get('/inventory', [POSController::class, 'inventory'])->name('inventory');
+    Route::get('/orders', [POSController::class, 'orders'])->name('orders');
+    Route::get('/livesearch/{key?}', [POSController::class, 'livesearch'])->name('livesearch');
+    Route::get('/item-search/{key?}', [POSController::class, 'itemSearch'])->name('item_search');
+    Route::get('/supplier-name/{name}', [POSController::class, 'ordersFromSupplier'])->name('supplier_name');
+    Route::post('/complete-order', [POSController::class, 'completeOrder'])->name('complete_order');
 });
 
 Route::get('/back-office/login', [OfficeController::class, 'adminLogin'])->name('office.login');
@@ -36,7 +41,8 @@ Route::get('add_admin', [AuthController::class, 'addAdmin']);
 Route::middleware([IsAdminLoggedIn::class])->group(function(){
     Route::get('/back-office/dashboard', [OfficeController::class, 'dashboard'])->name('office.dashboard');
     Route::get('/back-office/inventory', [OfficeController::class, 'inventory'])->name('office.inventory');
-    Route::post('back-office/logout', [AuthController::class, 'adminLogout'])->name('office.logout');
+    Route::get('back-office/logout', [AuthController::class, 'adminLogout'])->name('office.logout');
+    Route::get('/back-office/purchased_date', [OfficeController::class, 'purchasedDate'])->name('office.purchased_date');
     Route::get('/back-office/stocks_adjustments', [OfficeController::class, 'stocksAdjustment'])->name('office.stocks_adjustment');
     Route::get('/back-office/items_list', [OfficeController::class, 'itemsList'])->name('office.items_list');
     Route::get('/back-office/create_item', [OfficeController::class, 'createItem'])->name('office.create_item');
@@ -54,6 +60,12 @@ Route::middleware([IsAdminLoggedIn::class])->group(function(){
     Route::get('/back-office/stocks_adjustments/{item_name}', [OfficeController::class, 'adjustStocks'])->name('office.adjust');
     Route::get('/back-office/cashiers', [OfficeController::class, 'cashiers'])->name('office.cashiers');
     Route::post('/back-office/add_cashier', [AuthController::class, 'addCashier'])->name('office.add_cashier');
+    Route::get('/back-office/supplier', [OfficeController::class, 'suppliers'])->name('office.supplier');
+    Route::post('/back-office/add-supplier', [OfficeController::class, 'addSuppliers'])->name('office.add_supplier');
+    Route::get('/back-office/ordering', [OfficeController::class, 'ordering'])->name('office.ordering');
+    Route::get('/back-office/item_search/{key?}', [OfficeController::class, 'itemSearch'])->name('office.item_search');
+    Route::get('/back-office/supplier_search/{key?}', [OfficeController::class, 'supplierSearch'])->name('office.supplier_search');
+    Route::POST('/back-office/place-order', [OfficeController::class, 'placeOrder'])->name('office.place_order');
 });
 
 
